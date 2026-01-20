fd8965900c79ce7ef34af7d840a5d8f59aae2fb5
OakFoil <elsennahway@gmail.com>
2026-01-20 20:52:25 +0200

    feat: add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
+import Compiler.Parsing.Parser
+import Compiler.Parsing.Types
+import Data.Either
+import Data.Either.Extra (fromLeft')
+import Test.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = runTestTT parsingTests >>= print
+
+parsingTests :: Test
+parsingTests =
+  TestList $
+    map
+      testParsing
+      [ "from package import variable",
+        "a = a",
+        "1",
+        "lambda a, b: a + b",
+        "f(a, b, c)"
+      ]
+
+testParsing :: Input -> Test
+testParsing string = TestCase $ assertBool ("Got: " ++ fromLeft' parseResult) $ isRight parseResult
+  where
+    parseResult = runMyParser file "main.py" string
